9ab905866b13af762cff73ca66a5d5e51949afe6
Radu Lăncrănjan <radu.lancra@gmail.com>
2020-05-17 13:10:08 +0300

    Update message on viewModel update

--- WPFExpl/LoginViewModel.cs
+++ WPFExpl/LoginViewModel.cs
@@ -1,18 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WPFExpl
 {
-    class LoginViewModel
+    class LoginViewModel : INotifyPropertyChanged
     {
+
         public string Message { get; set; }
 
         public LoginViewModel()
         {
             Message = "User not logged in.";
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
--- WPFExpl/MainWindow.xaml.cs
+++ WPFExpl/MainWindow.xaml.cs
@@ -31,12 +31,14 @@ namespace WPFExpl
                 textboxUsername.Visibility = Visibility.Hidden;
                 textboxPassword.Visibility = Visibility.Hidden;
                 viewModel.Message = "Welcome, " + username;
+                viewModel.OnPropertyChanged(nameof(viewModel.Message));
             }
             else
             {
                 textboxUsername.Clear();
                 textboxPassword.Clear();
                 viewModel.Message = "Wrong credentials";
+                viewModel.OnPropertyChanged(nameof(viewModel.Message));
             }
         }
     }
