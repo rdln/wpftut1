885d3314467e2c7a5e030c5c73201b79c19d274b
Radu Lăncrănjan <radu.lancra@gmail.com>
2020-05-17 12:05:45 +0300

    Added LoginViewModel

---
+++ WPFExpl/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFExpl
+{
+    class LoginViewModel
+    {
+        public string Message { get; set; }
+
+        public LoginViewModel()
+        {
+            Message = "User not logged in.";
+        }
+    }
+}
--- WPFExpl/MainWindow.xaml.cs
+++ WPFExpl/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using WPFExpl.Services;
 
 namespace WPFExpl
@@ -23,6 +10,10 @@ namespace WPFExpl
     {
         public MainWindow()
         {
+            var viewModel = new LoginViewModel();
+
+            DataContext = viewModel;
+
             InitializeComponent();
         }
 
@@ -37,13 +28,13 @@ namespace WPFExpl
             {
                 textboxUsername.Visibility = Visibility.Hidden;
                 textboxPassword.Visibility = Visibility.Hidden;
-                this.labelUsermessage.Content = "Welcome, " + username;
+                //this.labelUsermessage.Content = "Welcome, " + username;
             }
             else
             {
                 textboxUsername.Clear();
                 textboxPassword.Clear();
-                this.labelUsermessage.Content = "Wrong credentials";
+                //this.labelUsermessage.Content = "Wrong credentials";
             }
         }
     }
