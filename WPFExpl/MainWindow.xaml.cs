133cf5d93b3d223abcc9986103621b0cf7d22ad7
Radu Lăncrănjan <radu.lancra@gmail.com>
2020-05-17 11:47:53 +0300

    initial commit.

---
+++ WPFExpl/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFExpl.Services;
+
+namespace WPFExpl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            var username = textboxUsername.Text;
+            var password = textboxPassword.Text;
+
+            var loginResult = AuthService.Instance.Login(username, password);
+
+            if (loginResult)
+            {
+                textboxUsername.Visibility = Visibility.Hidden;
+                textboxPassword.Visibility = Visibility.Hidden;
+                this.labelUsermessage.Content = "Welcome, " + username;
+            }
+            else
+            {
+                textboxUsername.Clear();
+                textboxPassword.Clear();
+                this.labelUsermessage.Content = "Wrong credentials";
+            }
+        }
+    }
+}
---
+++ WPFExpl/Services/AuthService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFExpl.Services
+{
+    public class AuthService
+    {
+        private static AuthService instance = null;
+        private static readonly object padlock = new object();
+
+        private AuthService()
+        {
+
+        }
+
+        public bool Login(string username, string password) =>
+            string.Compare(username, "a") == 0 
+            && string.Compare(password, "b") == 0;
+
+        public static AuthService Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new AuthService();
+                    }
+                }
+
+                return instance;
+            }
+        }
+    }
+}
